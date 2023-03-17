ca576f14c99b754324980c7167936a533e448ac8
Dylan Fanner <dylanfanner@gmail.com>
2023-03-17 13:32:12 +0200

    For bella <3

--- Assignment1.py
+++ Assignment1.py
@@ -1,70 +1,95 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Case to work with
+# Start time & end time
 
- 
-# Function definitions
-
-def cent_dif(y, n):
-    
-    next_pos = (((m/(h*h))*(2*y[n] - y[n-1])) + ((c/(2*h))*y[n-1]) + (-k*y[n]))/((m/(h*h)) + (c/(2*h)))
-    y.append(next_pos)
-
-    return y
-
-def force():
-    return 0
-
-# Physical constants
-
-m = 50
-c = 10
-k = 250
-
-m_0 = 6
-e = 0.225
-
-# Time definitions
-
-n_tot = 10000
-
+n_tot = 1000
 start_time = 0
 end_time = 10
 
+# Options: noDamp, damp, sinF, omegaF
+
+mode = 'sinF'
+
+if mode == 'userDef':
+    def force(n):
+        return 30*np.sin((n*h))
+    
+    [m, c, k] = [50, 10, 200]
+    [y_0, v_0] = [5, 3]
+
+    m_0 = 6
+    e = 0.225
+    
+    [y_0, v_0] = [1, 0]
+    
+elif mode == 'noDamp':
+    def force(t):
+        return 0
+    
+    [m, c, k] = [50, 0, 250]
+    [y_0, v_0] = [1, 2]
+    
+    # Real soln
+    x_s = np.arange(start_time, end_time, 0.01)
+    y_s = np.cos(np.sqrt(5)*x_s) + (2/np.sqrt(5))*np.sin(np.sqrt(5)*x_s)
+    
+elif mode == 'damp':
+    force = lambda t : 0
+    
+    [m, c, k] = [50, 10, 150]
+    [y_0, v_0] = [1, 2]
+    
+    # Real soln
+    x_s = np.arange(start_time, end_time, 0.01)
+    y_s = (np.cos(np.sqrt(299)/10*x_s) + (21/np.sqrt(299))*np.sin(np.sqrt(299)/10*x_s))*np.exp((-1/10)*x_s)
+    
+elif mode == 'sinF':
+    def force(n):
+        return 10*np.sin((n*h))
+    
+    [m, c, k] = [1, 5, 6]
+    [y_0, v_0] = [0, 5]
+    
+    # Real soln
+    x_s = np.arange(start_time, end_time, 0.01)
+    y_s = (-6*np.exp(-3*x_s) + 7*np.exp(-2*x_s) + np.sin(x_s) - np.cos(x_s))
+
+# Function definitions
+
+def cent_dif(y, n):
+    y[n+1] = (((m/(h*h))*(2*y[n] - y[n-1])) + ((c/(2*h))*y[n-1]) + (-k*y[n]) + force(n))/((m/(h*h)) + (c/(2*h)))
+    return y
+
+# Critical array definitions
+
 h = (end_time - start_time)/n_tot
+t = np.linspace(start_time, end_time, n_tot)
+t = np.append(t, -h)
 
-t_plot = [0, 0]
-t_plot[0] = 0 - h
-
-
-# Initial conditions
-
-y_0 = 1
-v_0 = 0
-
-# y_neg1 = y_0 - (v_0 - h*((force() - c*v_0 - k*y_0)/m))
-# First step foward
- 
-y = [0.9997, y_0]
-print(y)
-
+y = np.zeros(n_tot+1)
+y[0] = y_0
+y[-1] = y_0 - (v_0 - h*((force(0) - c*v_0 - k*y_0)/m))*h
 
 # Main loop
 
-t = start_time
-n = 1
+n = 0
 
-while (t < end_time):
-    
-    t = n*h
-    
-    y = cent_dif(y, n)
-    t_plot.append(t)
-    
+forces = []
+
+plt.figure(dpi=300)
+
+while (n < n_tot):
+    y = cent_dif(y, n)    
     n += 1 
 
-# Print plot
+# Plot output
 
-fig, ax = plt.subplots()
-ax.plot(t_plot, y)
+if mode != 'userDef':
+    plt.plot(x_s, y_s, color='darkmagenta')
+    
+#if mode == 'sinF':
+#    plt.plot(t[:-1], force(t[:])[:-1])
+
+plt.scatter(t[:-1], y[:-1], s=1, color='violet', marker='o')
+plt.show()
